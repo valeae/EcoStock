2bb20c1e89d4aee70323796508d9d5145d50126c
Valeae <vaescobar95.ve@gmail.com>
2024-08-29 10:09:35 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.html',
+    './index.html',
+    // Agrega aquí cualquier otra ruta donde tengas tus archivos HTML
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
